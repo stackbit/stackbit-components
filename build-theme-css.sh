463dfeba68e1e15cc44b309b7ee789c322383698
TomasBankauskas <tbdizainas@gmail.com>
2021-07-19 18:26:16 +0300

    removed kitwind.js

--- build-theme-css.sh
+++ build-theme-css.sh
@@ -8,5 +8,5 @@ themes=(
 for theme in "${themes[@]}"; do
   echo ""
   echo "Building CSS for '$theme' theme..."
-  tailwindcss build --minify --config "./themes/tailwind.$theme.config.js" --output "./public/css/tailwind.$theme.css"
+  tailwindcss build --minify -i "./src/css/global.css" --config "./themes/tailwind.$theme.config.js" --output "./public/css/tailwind.$theme.css"
 done

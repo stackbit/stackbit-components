2edd345ba17464845f6f37b33016796b75490fc2
Robert Austin <rob@jugglerdigital.com>
2021-08-03 17:47:54 +1000

    add index to components folder

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import HeroSection from './hero-section';
+import TestimonialsSection from './testimonials-section';
+import PostsSection from './posts-section';
+
+export default {
+  HeroSection,
+  TestimonialsSection,
+  PostsSection
+};

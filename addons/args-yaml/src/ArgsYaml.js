6da056c4bad9a0b7a12979f64ab351d6d7c099d0
Robert Austin <rob@jugglerdigital.com>
2021-08-30 20:23:47 +1000

    swap to js-yaml module

--- addons/args-yaml/src/ArgsYaml.js
+++ addons/args-yaml/src/ArgsYaml.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
-import YAML from 'json-to-pretty-yaml';
+import yaml from 'js-yaml';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './style.css';
 
 const ArgsYaml = (props) => {
     const { args, title, docsPage } = props;
-    const yaml = YAML.stringify(args);
-    const frontmatter = `---\n${yaml}\n---`;
+    const yamlObject = yaml.dump(args);
     const [copied, setCopied] = useState(false);
     return (
         <div className="args-yaml">
             {title && <h2>{title}</h2>}
             <div className={docsPage ? 'args-yaml-docs' : ''}>
                 <div className="args-yaml-copy">
-                    <CopyToClipboard text={yaml} onCopy={() => setCopied(true)}>
+                    <CopyToClipboard text={yamlObject} onCopy={() => setCopied(true)}>
                         <button className="args-yaml-button">Copy Code</button>
                     </CopyToClipboard>
                 </div>
                 <div className="args-yaml-code">
                     <code>
-                        <pre>{yaml}</pre>
+                        <pre>{yamlObject}</pre>
                     </code>
                 </div>
             </div>

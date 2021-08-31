d51bb03ab6d09f72078d379f9203335ca8b893b5
Simon Hanukaev <semenh@gmail.com>
2021-08-31 19:50:00 +0300

    added comment about dynamicComponents

--- src/components-registry.js
+++ src/components-registry.js
@@ -1,4 +1,7 @@
-// shadowing of dynamic-components.js is not working when @stackbit/components is in node_modules
+// When stackbit components are imported into a project,
+// withStackbitComponents() method replaces this import using Webpack resolve.
+// Instead of loading ./dynamic-components.js relative to this file
+// Webpack replaces it with a file located at /.stackbit/dynamic-components.js
 import dynamicComponents from './dynamic-components';
 
 export function registerDynamicComponents(newComponents) {

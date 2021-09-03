114450f3c07aa5e8f6fa4fde5f5a3bcbff188d28
Artem Denysov <denysov.artem@gmail.com>
2021-09-03 22:44:17 +0300

    Improve contact form to support async submissions

--- src/components/FormBlock/index.js
+++ src/components/FormBlock/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 import { getDynamicComponent } from '../../components-registry';
+
+const formHandler = (data, url) => {
+    return axios({
+        method: 'post',
+        url,
+        data
+    });
+};
+
+const handleSubmit = (event, formAction) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const value = Object.fromEntries(data.entries());
+    formHandler(value, formAction);
+};
 
 export default function FormBlock(props) {
     const fields = props.fields || [];
     if (fields.length === 0) {
         return null;
     }
+    const formHoneypotName = `${props.idAttr}-bot-field`;
     return (
         <form
             name={props.idAttr}
             id={props.idAttr}
-            {...(props.action ? { action: props.action } : null)}
-            method="POST"
+            onSubmit={(e) => handleSubmit(e, props.action)}
+            data-netlify="true"
+            data-netlify-honeypot={formHoneypotName}
             data-sb-field-path=".idAttr#@name .idAttr#@id .action#@action"
         >
             <div className="flex flex-wrap -mx-2" data-sb-field-path=".fields">
+                <input type="hidden" name="form-name" value={props.idAttr} />
+                <input type="hidden" name="form-destination" value={props.destination} />
                 {fields.map((field, index) => {
                     const fieldType = field.type;
                     if (!fieldType) {

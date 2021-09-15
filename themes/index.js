caa13a5717039d44f40e8805972c695e948863a5
Robert Austin <rob@jugglerdigital.com>
2021-09-15 17:36:06 +1000

    add example retro theme for testing

---
+++ themes/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    default: require('./default/tailwind.default.config'),
+    retro: require('./retro/tailwind.retro.config')
+};
---
+++ themes/retro/tailwind.retro.config.js
@@ -0,0 +1,37 @@
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    mode: 'jit',
+    purge: {
+        enabled: false,
+        content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@stackbit/components/{base,layouts,components}/**/*.{js,ts,jsx,tsx}'],
+        safelist: ['colors-a', 'colors-b', 'colors-c', 'colors-d', 'colors-e', 'colors-f']
+    },
+    darkMode: false,
+    theme: {
+        extend: {
+            colors: {
+                primary: '#512D6D',
+                secondary: '#F8485E',
+                base: '#EEEEEE',
+                'complimentary-1': '#e8f3ee',
+                'complimentary-2': '#e2e4ff',
+                info: '#00C1D4'
+            },
+            fontFamily: {
+                sans: ['Poppins', ...defaultTheme.fontFamily.serif]
+            },
+            spacing: {
+                '1/1': '100%',
+                '1/3': '33.333%',
+                '1/2': '50%',
+                '2/3': '66.667%'
+            }
+        }
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+};

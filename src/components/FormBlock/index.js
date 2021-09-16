7804d0cfc6720d153111f42d6e16525aea3b080d
Artem Denysov <denysov.artem@gmail.com>
2021-09-16 14:59:07 +0300

    Form fixes

--- src/components/FormBlock/index.js
+++ src/components/FormBlock/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import * as React from 'react';
 import axios from 'axios';
 import { getDynamicComponent } from '../../components-registry';
 
 export default class FormBlock extends React.Component {
     state = {
-        isSubmitting: false,
-        error: null
+        submitted: false,
+        error: false
     };
+
+    formRef = React.createRef();
 
     formHandler(data, url) {
         return axios({
@@ -19,37 +21,52 @@ export default class FormBlock extends React.Component {
     handleSubmit(event, formAction) {
         event.preventDefault();
 
-        const data = new FormData(event.currentTarget);
+        const data = new FormData(this.formRef.current);
         const value = Object.fromEntries(data.entries());
+
         this.setState({
-            isSubmitting: true,
-            error: null
+            submitted: false,
+            error: false
         });
 
         this.formHandler(value, formAction)
+            .then(() => {
+                this.setState({
+                    submitted: true
+                });
+                this.formRef.current.reset();
+            })
             .catch(() => {
                 this.setState({
-                    error: 'Error submitting form'
-                });
-            })
-            .finally(() => {
-                this.setState({
-                    isSubmitting: false
+                    error: 'Server error of handling form submission'
                 });
             });
     }
 
-    render({ fields = [], idAttr, action, destination, submitLabel, className }) {
+    componentDidUpdate(prevProps, prevState) {
+        if (!prevState.submitted && this.state.submitted) {
+            setTimeout(() => {
+                this.setState({
+                    submitted: false
+                });
+            }, 3000);
+        }
+    }
+
+    render() {
+        const { fields = [], idAttr, action, destination, submitLabel, className } = this.props;
         if (fields.length === 0) {
             return null;
         }
         const formHoneypotName = `${idAttr}-bot-field`;
         return (
             <form
+                className={className}
                 name={idAttr}
                 id={idAttr}
-                onSubmit={(e) => handleSubmit(e, action)}
+                onSubmit={(e) => this.handleSubmit(e, action)}
                 data-netlify="true"
+                ref={this.formRef}
                 data-netlify-honeypot={formHoneypotName}
                 data-sb-field-path=".idAttr#@name .idAttr#@id .action#@action"
             >
@@ -72,8 +89,9 @@ export default class FormBlock extends React.Component {
                     <button type="submit" className="sb-btn sb-btn-primary" data-sb-field-path=".submitLabel">
                         {submitLabel}
                     </button>
+                    {this.state.submitted && <span className="ml-8">Form successfully submitted</span>}
+                    {this.state.error && <span className="ml-8 text-info">Server error of handling form submission</span>}
                 </div>
-                <div className="mt-4 sm:mt-8">{this.state.error}</div>
             </form>
         );
     }

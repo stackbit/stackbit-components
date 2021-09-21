1db2993db0ad8520f7b5683fce892a296a23f632
tomasbankauskas <tbdizainas@gmail.com>
2021-09-21 12:27:58 +0300

    fixed broken image link

--- src/layouts/PostLayout/post-layout.stories.js
+++ src/layouts/PostLayout/post-layout.stories.js
@@ -26,14 +26,14 @@ Primary.storyName = 'Layout';
 Primary.args = {
     page: {
         __metadata: {},
-        title: 'Sustainability at it’s purest',
+        title: 'Post Title',
         layout: 'PostLayout',
         date: '2021-07-03',
-        excerpt: 'We’re local, seasonal fisherman, supporting fishing restrictions. We fish what the sea has to offer, nothing more, and no cheating.',
+        excerpt: 'Gracchum patrem non beatiorem fuisse quam fillum, cum alter stabilire rem publicam studuerit, alter evertere. Quo plebiscito decreta a senatu est consuli quaestio.',
         thumbImage: {
             type: 'ImageBlock',
-            url: '/images/fisherman.jpg',
-            altText: 'Fisherman'
+            url: '/images/post-1.jpeg',
+            altText: 'Post Image'
         },
         markdown_content:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Duo Reges: constructio interrete. An hoc usque quaque, aliter in vita? Gracchum patrem non beatiorem fuisse quam fillum, cum alter stabilire rem publicam studuerit, alter evertere. Quo plebiscito decreta a senatu est consuli quaestio Cn. Illud non continuo, ut aeque incontentae. Atqui pugnantibus et contrariis studiis consiliisque semper utens nihil quieti videre, nihil tranquilli potest. Itaque hoc frequenter dici solet a vobis, non intellegere nos, quam dicat Epicurus voluptatem. Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret.\n\n## Sed Ille, UT Dixi, Vitiose\n\nUtrum igitur tibi litteram videor an totas paginas commovere? Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Ne in odium veniam, si amicum destitero tueri. Ne amores quidem sanctos a sapiente alienos esse arbitrantur. Quid ergo aliud intellegetur nisi uti ne quae pars naturae neglegatur? Quis istud, quaeso, nesciebat? Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam aut expetendam. Et ille ridens: Video, inquit, quid agas; In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret.\n\n',

49c57d745f2540383c3176805d2769f128b4c8e1
Artem Denysov <denysov.artem@gmail.com>
2021-09-22 20:41:27 +0300

    Feature/contact form (#18)
    Form Ui updates

--- src/components/FormBlock/index.js
+++ src/components/FormBlock/index.js
@@ -1,58 +1,102 @@
-import React from 'react';
+import * as React from 'react';
 import axios from 'axios';
 import { getDynamicComponent } from '../../components-registry';
 
-const formHandler = (data, url) => {
-    return axios({
-        method: 'post',
-        url,
-        data
-    });
-};
+export default class FormBlock extends React.Component {
+    state = {
+        submitted: false,
+        error: false
+    };
 
-const handleSubmit = (event, formAction) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const value = Object.fromEntries(data.entries());
-    formHandler(value, formAction);
-};
+    formRef = React.createRef();
 
-export default function FormBlock(props) {
-    const fields = props.fields || [];
-    if (fields.length === 0) {
-        return null;
+    formHandler(data, url) {
+        return axios({
+            method: 'post',
+            url,
+            data
+        });
     }
-    const formHoneypotName = `${props.idAttr}-bot-field`;
-    return (
-        <form
-            className={props.className}
-            name={props.idAttr}
-            id={props.idAttr}
-            onSubmit={(e) => handleSubmit(e, props.action)}
-            data-netlify="true"
-            data-netlify-honeypot={formHoneypotName}
-            data-sb-field-path=".idAttr#@name .idAttr#@id .action#@action"
-        >
-            <div className="flex flex-wrap -mx-2" data-sb-field-path=".fields">
-                <input type="hidden" name="form-name" value={props.idAttr} />
-                <input type="hidden" name="form-destination" value={props.destination} />
-                {fields.map((field, index) => {
-                    const fieldType = field.type;
-                    if (!fieldType) {
-                        throw new Error(`form field does not have the 'type' property`);
-                    }
-                    const FormControl = getDynamicComponent(fieldType);
-                    if (!FormControl) {
-                        throw new Error(`no component matching the form field type: ${fieldType}`);
-                    }
-                    return <FormControl key={index} {...field} annotationPrefix={`.${index}`} />;
-                })}
-            </div>
-            <div className="mt-4 sm:mt-8">
-                <button type="submit" className="sb-component sb-component-block sb-component-btn sb-component-btn-primary" data-sb-field-path=".submitLabel">
-                    {props.submitLabel}
-                </button>
-            </div>
-        </form>
-    );
+
+    handleSubmit(event, formAction) {
+        event.preventDefault();
+
+        const data = new FormData(this.formRef.current);
+        const value = Object.fromEntries(data.entries());
+
+        this.setState({
+            submitted: false,
+            error: false
+        });
+
+        this.formHandler(value, formAction)
+            .then(() => {
+                this.setState({
+                    submitted: true
+                });
+                this.formRef.current.reset();
+            })
+            .catch(() => {
+                this.setState({
+                    error: true
+                });
+            });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (!prevState.submitted && this.state.submitted) {
+            setTimeout(() => {
+                this.setState({
+                    submitted: false
+                });
+            }, 3000);
+        }
+    }
+
+    render() {
+        const { fields = [], idAttr, action, destination, submitLabel, className } = this.props;
+        if (fields.length === 0) {
+            return null;
+        }
+        const formHoneypotName = `${idAttr}-bot-field`;
+        return (
+            <form
+                className={className}
+                name={idAttr}
+                id={idAttr}
+                onSubmit={(e) => this.handleSubmit(e, action)}
+                data-netlify="true"
+                ref={this.formRef}
+                data-netlify-honeypot={formHoneypotName}
+                data-sb-field-path=".idAttr#@name .idAttr#@id .action#@action"
+            >
+                <div className="flex flex-wrap -mx-2" data-sb-field-path=".fields">
+                    <input type="hidden" name="form-name" value={idAttr} />
+                    <input type="hidden" name="form-destination" value={destination} />
+                    {fields.map((field, index) => {
+                        const fieldType = field.type;
+                        if (!fieldType) {
+                            throw new Error(`form field does not have the 'type' property`);
+                        }
+                        const FormControl = getDynamicComponent(fieldType);
+                        if (!FormControl) {
+                            throw new Error(`no component matching the form field type: ${fieldType}`);
+                        }
+                        return <FormControl key={index} {...field} annotationPrefix={`.${index}`} />;
+                    })}
+                </div>
+                <div className="mt-4 sm:mt-8">
+                    <button
+                        type="submit"
+                        className="sb-component sb-component-block sb-component-btn sb-component-btn-primary"
+                        data-sb-field-path=".submitLabel"
+                    >
+                        {submitLabel}
+                    </button>
+                    {this.state.submitted && <span className="ml-8">Thank you, your message was sent.</span>}
+                    {this.state.error && <span className="ml-8 text-info">Something went wrong, please try again.</span>}
+                </div>
+            </form>
+        );
+    }
 }

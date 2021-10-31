c40d1fd68a881cb958d6da03d10dd09fe694fdd1
Tomas Bankauskas <tbdizainas@gmail.com>
2021-10-31 08:40:27 +0200

    Added SEO support for posts and pages (#46)

--- src/base/BlankBaseLayout/index.tsx
+++ src/base/BlankBaseLayout/index.tsx
@@ -3,13 +3,40 @@ import Head from 'next/head';
 import classNames from 'classnames';
 
 export default function BlankBaseLayout(props) {
-    const { page } = props;
+    const { page, site } = props;
     const pageMeta = page?.__metadata || {};
+    const domain = (site?.domain || '').replace(/\/+$/g, '');
+    const seo = page?.seo || {};
+    const seoTitle = seo.title ? seo.title : page.title;
+    const seoDescription = seo.description || '';
+    const seoRobots = (seo.robots || []).join(',');
+    const seoExtra = (seo.extra || []).map((meta, index) => {
+        const keyName = meta.keyName || 'name';
+        const name = meta.name;
+        if (!name) {
+            return null;
+        }
+        const nameAttr = { [keyName]: name };
+        const relativeUrl = meta.relativeUrl || false;
+        let value = meta.value;
+        if (!value) {
+            return null;
+        }
+        if (relativeUrl) {
+            if (!domain) {
+                return null;
+            }
+            value = domain + '/' + value.replace(/^\/+/g, '');
+        }
+        return <meta key={index} {...nameAttr} content={value} />;
+    });
     return (
         <div className={classNames('page', pageMeta.pageCssClasses)} data-sb-object-id={pageMeta.id}>
             <Head>
-                <title>{page.title}</title>
-                <meta name="description" content="Stackbit Components Library" />
+                <title>{seoTitle}</title>
+                <meta name="description" content={seoDescription} />
+                {seoRobots && <meta name="robots" content={seoRobots} />}
+                {seoExtra}
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
             </Head>
--- src/layouts/PageLayout/page-layout.stories.tsx
+++ src/layouts/PageLayout/page-layout.stories.tsx
@@ -43,9 +43,54 @@ Primary.args = {
             QuoteSectionStories.Primary.args,
             TextSectionStories.Primary.args,
             TestimonialsSectionStories.Primary.args
-        ]
+        ],
+        seo: {
+            title: 'Stackbit Starter Theme',
+            description: 'The preview of the Starter theme',
+            extra: [
+                {
+                    name: 'og:type',
+                    value: 'website',
+                    keyName: 'property'
+                },
+                {
+                    name: 'og:title',
+                    value: 'Stackbit Starter Theme',
+                    keyName: 'property'
+                },
+                {
+                    name: 'og:description',
+                    value: 'The preview of the Starter theme',
+                    keyName: 'property'
+                },
+                {
+                    name: 'og:image',
+                    value: '/images/hero.png',
+                    keyName: 'property',
+                    relativeUrl: true
+                },
+                {
+                    name: 'twitter:card',
+                    value: 'summary_large_image'
+                },
+                {
+                    name: 'twitter:title',
+                    value: 'Stackbit Starter Theme'
+                },
+                {
+                    name: 'twitter:description',
+                    value: 'The preview of the Starter theme'
+                },
+                {
+                    name: 'twitter:image',
+                    value: '/images/hero.png',
+                    relativeUrl: true
+                }
+            ]
+        }
     },
     site: {
+        domain: 'https://mysite.com/',
         header: HeaderStories.Primary.args,
         footer: FooterStories.Primary.args
     }

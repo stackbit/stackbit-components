f76ede904ed7bb885f3176a1b874dbe781f8d9a6
Robert Austin <rob@jugglerdigital.com>
2021-11-16 18:01:35 +1000

    alert4

--- src/components/AlertSection/index.tsx
+++ src/components/AlertSection/index.tsx
@@ -30,7 +30,7 @@ export default function AlertSection(props) {
             }}
             data-sb-field-path={props.annotationPrefix}
         >
-            <h1>Alert3</h1>
+            <h1>Alert4</h1>
             <p>{props.body}</p>
         </div>
     );

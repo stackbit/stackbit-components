29f6768f0443be2a4ca6181efaf644a7bc2eb393
Tomas Bankauskas <tbdizainas@gmail.com>
2021-11-16 16:07:44 +0200

    removed feat image in post, added optional author, date in feat posts section (#62)

--- src/components/FeaturedPostsSection/featured-posts-section.stories.tsx
+++ src/components/FeaturedPostsSection/featured-posts-section.stories.tsx
@@ -18,6 +18,14 @@ export default {
             options: ['colors-a', 'colors-b', 'colors-c', 'colors-d', 'colors-e', 'colors-f', 'colors-g', 'colors-h', 'colors-i'],
             defaultValue: 'colors-a',
             control: { type: 'select' }
+        },
+        showDate: {
+            defaultValue: false,
+            control: { type: 'boolean' }
+        },
+        showAuthor: {
+            defaultValue: false,
+            control: { type: 'boolean' }
         }
     }
 };
@@ -29,6 +37,8 @@ const args = {
     elementId: '',
     variant: 'variant-a',
     colors: 'colors-f',
+    showDate: true,
+    showAuthor: true,
     title: 'Featured Posts',
     subtitle: 'This is the Subtitle',
     actions: [
@@ -44,6 +54,17 @@ const args = {
             __metadata: {},
             title: 'Post Title One',
             date: '2021-06-28',
+            author: {
+                firstName: 'Desmond',
+                lastName: 'Eagle',
+                role: 'Product Manager',
+                bio: 'Nullam rhoncus tellus nec felis ullamcorper mattis. Cras convallis, velit in tincidunt posuere, nulla ipsum consequat metus, convallis lacinia ex eros eget leo.',
+                image: {
+                    type: 'ImageBlock',
+                    url: '/images/desmond-eagle.jpg',
+                    altText: 'Photo of Desmond Eagle'
+                }
+            },
             excerpt: 'Quo plebiscito decreta a senatu est consuli quaestio Cn. Illud non continuo, ut aeque incontentae. Atqui pugnantibus et contrariis.',
             featuredImage: {
                 type: 'ImageBlock',
@@ -57,6 +78,17 @@ const args = {
             __metadata: {},
             title: 'Post Title Two',
             date: '2021-07-01',
+            author: {
+                firstName: 'Hilary',
+                lastName: 'Ouse',
+                role: 'Product Manager',
+                bio: 'Vincent Van Gogh’s most popular painting, The Starry Night.',
+                image: {
+                    type: 'ImageBlock',
+                    url: '/images/hilary-ouse.jpg',
+                    altText: 'Photo of Hilary Ouse'
+                }
+            },
             excerpt: 'otius inflammat, ut coercendi magis quam dedocendi esse videantur.',
             featuredImage: {
                 type: 'ImageBlock',
@@ -70,6 +102,17 @@ const args = {
             __metadata: {},
             title: 'Post Title Three',
             date: '2021-07-03',
+            author: {
+                firstName: 'Hugh',
+                lastName: 'Saturation',
+                role: 'Product Manager',
+                bio: 'Vincent Van Gogh’s most popular painting, The Starry Night.',
+                image: {
+                    type: 'ImageBlock',
+                    url: '/images/hugh-saturation.jpg',
+                    altText: 'Photo of Hugh Saturation'
+                }
+            },
             excerpt: 'Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam.',
             featuredImage: {
                 type: 'ImageBlock',
@@ -83,6 +126,17 @@ const args = {
             __metadata: {},
             title: 'Post Title Four',
             date: '2021-11-03',
+            author: {
+                firstName: 'Hilary',
+                lastName: 'Ouse',
+                role: 'Product Manager',
+                bio: 'Vincent Van Gogh’s most popular painting, The Starry Night.',
+                image: {
+                    type: 'ImageBlock',
+                    url: '/images/hilary-ouse.jpg',
+                    altText: 'Photo of Hilary Ouse'
+                }
+            },
             excerpt: 'Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam.',
             featuredImage: {
                 type: 'ImageBlock',
@@ -127,47 +181,8 @@ export const Primary = Template.bind({});
 Primary.storyName = 'Featured Posts, Three Columns Grid';
 Primary.args = {
     ...args,
-    posts: [
-        {
-            __metadata: {},
-            title: 'Post Title One',
-            date: '2021-06-28',
-            excerpt: 'Quo plebiscito decreta a senatu est consuli quaestio Cn. Illud non continuo, ut aeque incontentae. Atqui pugnantibus et contrariis.',
-            featuredImage: {
-                type: 'ImageBlock',
-                url: '/images/post-1.jpeg',
-                altText: 'Post Image'
-            },
-            markdown_content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Duo Reges: constructio interrete. An hoc usque quaque, aliter in vita? Gracchum patrem non beatiorem fuisse quam fillum, cum alter stabilire rem publicam studuerit, alter evertere. Quo plebiscito decreta a senatu est consuli quaestio Cn. Illud non continuo, ut aeque incontentae. Atqui pugnantibus et contrariis studiis consiliisque semper utens nihil quieti videre, nihil tranquilli potest. Itaque hoc frequenter dici solet a vobis, non intellegere nos, quam dicat Epicurus voluptatem. Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret.\n\n## Sed Ille, UT Dixi, Vitiose\n\nUtrum igitur tibi litteram videor an totas paginas commovere? Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Ne in odium veniam, si amicum destitero tueri. Ne amores quidem sanctos a sapiente alienos esse arbitrantur. Quid ergo aliud intellegetur nisi uti ne quae pars naturae neglegatur? Quis istud, quaeso, nesciebat? Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam aut expetendam. Et ille ridens: Video, inquit, quid agas; In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret.\n\n'
-        },
-        {
-            __metadata: {},
-            title: 'Post Title Two',
-            date: '2021-07-01',
-            excerpt: 'otius inflammat, ut coercendi magis quam dedocendi esse videantur.',
-            featuredImage: {
-                type: 'ImageBlock',
-                url: '/images/post-2.jpeg',
-                altText: 'Post Image'
-            },
-            markdown_content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Duo Reges: constructio interrete. An hoc usque quaque, aliter in vita? Gracchum patrem non beatiorem fuisse quam fillum, cum alter stabilire rem publicam studuerit, alter evertere. Quo plebiscito decreta a senatu est consuli quaestio Cn. Illud non continuo, ut aeque incontentae. Atqui pugnantibus et contrariis studiis consiliisque semper utens nihil quieti videre, nihil tranquilli potest. Itaque hoc frequenter dici solet a vobis, non intellegere nos, quam dicat Epicurus voluptatem. Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret.\n\n## Sed Ille, UT Dixi, Vitiose\n\nUtrum igitur tibi litteram videor an totas paginas commovere? Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Ne in odium veniam, si amicum destitero tueri. Ne amores quidem sanctos a sapiente alienos esse arbitrantur. Quid ergo aliud intellegetur nisi uti ne quae pars naturae neglegatur? Quis istud, quaeso, nesciebat? Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam aut expetendam. Et ille ridens: Video, inquit, quid agas; In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret.\n\n'
-        },
-        {
-            __metadata: {},
-            title: 'Post Title Three',
-            date: '2021-07-03',
-            excerpt: 'Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam.',
-            featuredImage: {
-                type: 'ImageBlock',
-                url: '/images/post-3.jpeg',
-                altText: 'Post Image'
-            },
-            markdown_content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Duo Reges: constructio interrete. An hoc usque quaque, aliter in vita? Gracchum patrem non beatiorem fuisse quam fillum, cum alter stabilire rem publicam studuerit, alter evertere. Quo plebiscito decreta a senatu est consuli quaestio Cn. Illud non continuo, ut aeque incontentae. Atqui pugnantibus et contrariis studiis consiliisque semper utens nihil quieti videre, nihil tranquilli potest. Itaque hoc frequenter dici solet a vobis, non intellegere nos, quam dicat Epicurus voluptatem. Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret.\n\n## Sed Ille, UT Dixi, Vitiose\n\nUtrum igitur tibi litteram videor an totas paginas commovere? Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Ne in odium veniam, si amicum destitero tueri. Ne amores quidem sanctos a sapiente alienos esse arbitrantur. Quid ergo aliud intellegetur nisi uti ne quae pars naturae neglegatur? Quis istud, quaeso, nesciebat? Primum divisit ineleganter; Hoc unum Aristo tenuit: praeter vitia atque virtutes negavit rem esse ullam aut fugiendam aut expetendam. Et ille ridens: Video, inquit, quid agas; In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret.\n\n'
-        }
-    ]
+    showDate: false,
+    showAuthor: false
 };
 
 export const FeaturedPostsTwoCol = Template.bind({});

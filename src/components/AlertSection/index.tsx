160846b4051b7c0a15efc4d4d78cafa7c272fc65
Robert Austin <rob@jugglerdigital.com>
2021-11-17 16:19:40 +1000

    remove annotation

--- src/components/AlertSection/index.tsx
+++ src/components/AlertSection/index.tsx
@@ -1,47 +1,12 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import { mapStylesToClassNames as mapStyles } from '../../utils/map-styles-to-class-names';
 
 export default function AlertSection(props) {
     const cssId = props.elementId || null;
-    const colors = props.colors || 'colors-a';
-    const sectionStyles = props.styles?.self || {};
-    const sectionBorderWidth = sectionStyles.borderWidth ? sectionStyles.borderWidth : 0;
     return (
-        <div
-            id={cssId}
-            className={classNames(
-                'sb-component',
-                'sb-component-section',
-                'sb-component-alert-section',
-                colors,
-                'flex',
-                'flex-col',
-                'justify-center',
-                sectionStyles.height ? mapMinHeightStyles(sectionStyles.height) : null,
-                sectionStyles.margin,
-                sectionStyles.padding,
-                sectionStyles.borderColor,
-                sectionStyles.borderRadius ? mapStyles({ borderRadius: sectionStyles.borderRadius }) : null,
-                sectionStyles.borderStyle ? mapStyles({ borderStyle: sectionStyles.borderStyle }) : null
-            )}
-            style={{
-                borderWidth: `${sectionBorderWidth}px`
-            }}
-            data-sb-field-path={props.annotationPrefix}
-        >
-            <h1>Alert8</h1>
+        <div id={cssId} className={classNames('sb-component', 'sb-component-section', 'sb-component-alert-section')}>
+            <h1>Alert</h1>
             <p>{props.body}</p>
         </div>
     );
-}
-
-function mapMinHeightStyles(height) {
-    switch (height) {
-        case 'auto':
-            return 'min-h-0';
-        case 'screen':
-            return 'min-h-screen';
-    }
-    return null;
 }

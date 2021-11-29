4fd65bb082f3b25158d63853457bb9ffd61a3830
Tomas Bankauskas <tbdizainas@gmail.com>
2021-11-29 16:35:01 +0200

    updated FormBlock (#81)

--- src/components/FormBlock/index.tsx
+++ src/components/FormBlock/index.tsx
@@ -70,9 +70,9 @@ export default class FormBlock extends React.Component<any> {
                 data-netlify="true"
                 ref={this.formRef}
                 data-netlify-honeypot={formHoneypotName}
-                data-sb-field-path=".elementId#@name .elementId#@id .action#@action .fields"
+                data-sb-field-path=".elementId#@name .elementId#@id .action#@action"
             >
-                <div className="grid sm:grid-cols-2 sm:gap-x-4">
+                <div className="grid sm:grid-cols-2 sm:gap-x-4" data-sb-field-path=".fields">
                     <input type="hidden" name="form-name" value={elementId} />
                     <input type="hidden" name="form-destination" value={destination} />
                     {fields.map((field, index) => {
